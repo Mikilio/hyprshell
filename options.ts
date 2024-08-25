8bdcf12748d0c826d83968af317a6db76ffb8b87
Mikilio <official.mikilio@gmail.com>
2024-08-25 16:00:05 +0200

    chore: update with Aylurs changes

--- options.ts
+++ options.ts
@@ -60,7 +60,7 @@ const options = mkOptions(OPTIONS, {
   bar: {
     flatButtons: opt(true),
     position: opt<"top" | "bottom">("top"),
-    corners: opt(true),
+    corners: opt(50),
     transparent: opt(false),
     layout: {
       start: opt<Array<import("widget/bar/Bar").BarWidget>>([
